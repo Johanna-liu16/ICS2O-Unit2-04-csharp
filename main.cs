35db1d7f8dbfe724bba8078a8dc45cb16bb2ea07
Johanna-liu16 <99354992+Johanna-liu16@users.noreply.github.com>
2022-03-27 00:13:31 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,16 @@
+// Created by: Johanna Liu
+// Created on: Mar 2022
+//
+// This program does basic math
+
+using System;
+
+class Program {
+  public static void Main(string[] args)
+    {
+        // This function states user's address input.
+        string height;
+        int base;
+
+        Console.WriteLine("This program will retell you your street name and street number.");
+        Console.WriteLine("");
