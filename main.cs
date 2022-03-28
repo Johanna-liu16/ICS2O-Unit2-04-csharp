8c40d7a655716630ac1a27a45a238080d79a2870
Johanna-liu16 <99354992+Johanna-liu16@users.noreply.github.com>
2022-03-28 01:01:45 +0000

    whitespace

--- main.cs
+++ main.cs
@@ -5,23 +5,23 @@
 
 using System;
 
-class Program 
+class Program
 {
   public static void Main(string[] args)
     {
         // This function states user's address input.
         int triangleHeight;
         int triangleBase;
-      
-        Console.WriteLine("This program will retell the area of a triangle."); 
+
+        Console.WriteLine("This program will retell the area of a triangle.");
         Console.WriteLine("");
-        Console.Write("Enter height of the triangle: "); 
-        triangleHeight = Convert.ToInt32(Console.ReadLine()); 
+        Console.Write("Enter height of the triangle: ");
+        triangleHeight = Convert.ToInt32(Console.ReadLine());
         Console.Write("Enter base of the triangle: ");
         triangleBase = Convert.ToInt32(Console.ReadLine());
 
         Console.WriteLine("");
-        Console.WriteLine("The area is: " + (triangleHeight * triangleBase) /2  + " cm²."); 
-        Console.WriteLine("\nDone."); 
+        Console.WriteLine("The area is: " + (triangleHeight*triangleBase) /2  + " cm²."); 
+        Console.WriteLine("\nDone.");
     } 
-} 
+}
